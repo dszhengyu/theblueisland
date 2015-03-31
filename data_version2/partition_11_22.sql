e2b5051d9423dcbfe5396665c01d6d21fcae7814
Appendix <dszhengyu@qq.com>
2015-04-01 00:00:00 +0800

    complete itemFeature & userFeature, uiFeature to be continue

---
+++ data_version2/partition_11_22.sql
@@ -0,0 +1,43 @@
+set @d1 = str_to_date('2014-11-22 00', '%Y-%m-%d %H');
+set @d2 = str_to_date('2014-12-02 00', '%Y-%m-%d %H');
+set @d3 = str_to_date('2014-12-03 00', '%Y-%m-%d %H');
+
+CREATE TABLE `u_11_22` (
+  `user_id` varchar(20) NOT NULL,
+  `item_id` varchar(20) NOT NULL,
+  `behavior_type` varchar(1) NOT NULL,
+  `user_geohash` varchar(20) DEFAULT NULL,
+  `item_category` varchar(20) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `i_11_22` (
+  `user_id` varchar(20) NOT NULL,
+  `item_id` varchar(20) NOT NULL,
+  `behavior_type` varchar(1) NOT NULL,
+  `user_geohash` varchar(20) DEFAULT NULL,
+  `item_category` varchar(20) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `l_11_22` (
+  `user_id` varchar(20) NOT NULL,
+  `item_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into u_11_22
+	select *
+    from _train_user
+    where time >= @d1 and time < @d2
+    order by user_id, item_id;
+ 
+ insert into i_11_22
+	select *
+    from _train_user
+    where time >= @d1 and time < @d2
+    order by item_id ,user_id;
+    
+insert into l_11_22
+	select distinct user_id, item_id
+    from _train_user
+    where time >= @d2 and time < @d3 and behavior_type = 4;
--- extractFeature.py
+++ extractFeature.py
@@ -1,50 +1,48 @@
 #the split(",") func would return list like ['"abc"', '"..."', ..., '"..."\n']
-#to use the value as key or sth, you should call .strip('"') and
-#for the last element in list, call .strip('"\n"')
+#entry = [elm.strip('"\n') for elm in line.split(",")]
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
-def extractFeature(day, prefix = "data_version2\\"):
+def extractFeature(day, prefix = "Z:\\theblueisland\\data_version2\\"):
     ufile = prefix + "u_" + day + ".csv"
     ifile = prefix + "i_" + day + ".csv"
     lfile = prefix + "l_" + day + ".csv"
     featureDay = prefix + "feature_" + day + ".csv"
     labelDay = prefix + "label_" + day + ".csv"
+    uptime = datetime.strptime('2014_' + day + ' 00:00:00', "%Y_%m_%d %H:%M:%S")
+    uptime += timedelta(days = 9, hours = 23)
 
     beginTime = datetime.now()
-
-    #d = datetime.strptime(x[5].strip("'\n"), '"%Y-%m-%d %H:%M:%S"')
-
+    
 #load labelDict
     labelDict = {}
     l = open(lfile)
     for line in l.readlines():
-        entry = line.split(",")
-        userL = entry[0].strip('"')
-        itemL = entry[1].strip('"\n')
-        labelDict[(userL, itemL)] = None
+        entry = [elm.strip('"\n') for elm in line.split(",")]
+        labelDict[(entry[0], entry[1])] = None
     l.close()
-
+        
 #extract item feature & put into itemDict
     itemDict = {}
     i = open(ifile)
     line = i.readline()
-    curItem = line.split(",")[1]
+    entry = [elm.strip('"\n') for elm in line.split(",")]
+    curItem = entry[1]
     itemSet = []
-    itemSet.append(line.split(","))
+    itemSet.append(entry)
     for line in i.readlines():
-        entry = line.split(",")
+        entry = [elm.strip('"\n') for elm in line.split(",")]
         if (entry[1] == curItem):
             itemSet.append(entry)
         else:
-            iValue = selectItemFeature(itemSet)
-            itemDict[curItem.strip('"')] = iValue
+            iValue = selectItemFeature(itemSet, uptime)
+            itemDict[curItem] = iValue
             itemSet = []
             itemSet.append(entry)
             curItem = entry[1]
     else:
-        iValue = selectItemFeature(itemSet)
-        itemDict[curItem.strip('"')] = iValue
+        iValue = selectItemFeature(itemSet, uptime)
+        itemDict[curItem] = iValue
     i.close()
 
 #extract u-i & user feature, add item features
@@ -52,45 +50,80 @@ def extractFeature(day, prefix = "data_version2\\"):
     labels = []
     f = open(ufile)
     line = f.readline()
-    curUser = line.split(",")[0]
+    entry = [elm.strip('"\n') for elm in line.split(",")]
+    curUser = entry[0]
     userSet = []
-    userSet.append(line.split(","))
+    userSet.append(entry)
     for line in f.readlines():
-        entry = line.split(",")
+        entry = [elm.strip('"\n') for elm in line.split(",")]
         if (entry[0] == curUser):
             userSet.append(entry)
         else:
-            uValue = selectUserFeature(userSet)
-            uiValue, label = selectFeatureLabel(userSet, uValue, itemDict, labelDict)
+            uValue = selectUserFeature(userSet, uptime)
+            uiValue, label = selectFeatureLabel(userSet, uValue, itemDict, labelDict, uptime)
             features.extend(uiValue)
             labels.extend(label)
             userSet = []
             userSet.append(entry)
             curUser = entry[0]
     else:
-        uValue = selectUserFeature(userSet)
-        uiValue, label = selectFeatureLabel(userSet, uValue, itemDict, labelDict)
+        uValue = selectUserFeature(userSet, uptime)
+        uiValue, label = selectFeatureLabel(userSet, uValue, itemDict, labelDict, uptime)
         features.extend(uiValue)
         labels.extend(label)
     f.close()
 
     endTime = datetime.now()
     print "used time: " + str(endTime - beginTime)
+    input(">>")
 
-def selectItemFeature(itemSet):
+
+def selectItemFeature(itemSet, uptime):
     'method to select item feature'
-    return []
+    #write into table and dict
+    routine = [[0 for _ in range(5)] for _ in range(10)]
+    person = {}
+    for entry in itemSet:
+        deltaDay = (uptime - datetime.strptime(entry[5], "%Y-%m-%d %H:%M:%S")).days
+        behav = int(entry[2])
+        routine[deltaDay][behav] += 1
+        person.setdefault(entry[0], [0, 0, 0, 0, 0])
+        person[entry[0]][behav] += 1
+    #sum up the data in routine and dict
+    every1 = [routine[i][j] for i in range(10) for j in range (1, 5)]
+    every2 = [routine[i][j] + routine[i + 1][j] for i in range(0, 10, 2) for j in range(1, 5)]
+    every3 = [routine[i][j] + routine[i + 1][j] + routine[i + 2][j] for i in range(0, 9, 3) for j in range(1, 5)]
+    totalPerson = 0
+    total = [0, 0, 0, 0, 0]
+    for key in person:
+        total = [total[i] + person[key][i] for i in range(5)]
+        totalPerson += 1
+    return total[1 : 5] + [totalPerson] + every1 + every2 + every3
+           
 
-def selectUserFeature(userSet):
+def selectUserFeature(userSet, uptime):
     'method to select user feature'
-    return []
+    #write into dict
+    thing = {}
+    for entry in userSet:
+        behav = int(entry[2])
+        thing.setdefault(entry[1], [0, 0, 0, 0, 0])
+        thing[entry[1]][behav] += 1
+    #sum uo the data in dict
+    totalThing = 0
+    total = [0, 0, 0, 0, 0]
+    for key in thing:
+        total = [total[i] + thing[key][i] for i in range(5)]
+        totalThing += 1
+    return total[1 : 5] + [totalThing]
 
-def selectFeatureLabel(userSet, uValue, itemDict, labelDict):
+def selectFeatureLabel(userSet, uValue, itemDict, labelDict, uptime):
     'method to select feature, based on <user_id, Item_id>'
+    
     return [['uiValue']], [['label']]
 
 def test():
-    extractFeature("11_18");
+    extractFeature('11_18');
     
 
 if __name__ == '__main__': test()
