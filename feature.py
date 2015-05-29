893946552a4fdb07c3c453db1ba59f6e8f1c1768
Appendix <dszhengyu@qq.com>
2015-05-29 13:05:11 +0800

    to linux

--- feature.py
+++ feature.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from sklearn.grid_search import GridSearchCV
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 pwd = 'z:\\theblueisland\\'
 featureName = pwd + 'feature.csv'
@@ -14,6 +16,36 @@ user_profile_table = raw_data + 'user_profile_table.txt'
 day_share_interest_table = raw_data + 'mfd_day_share_interest.txt'
 mfd_bank_shibor_table = raw_data + 'mfd_bank_shibor.txt'
 
+def extract(X, y):
+    feature = extractX(X)
+    
+    y.loc[ : , 'dayOfMonth'] = y['report_date'].map(lambda unit: unit.day)
+    yPersonDayGroup = y.groupby(['user_id', 'dayOfMonth'])
+    target = yPersonDayGroup['total_purchase_amt', 'total_redeem_amt'].max()
+    
+    result = pd.merge(feature, target, how = 'right', left_index = True, right_index = True)
+    
+    #result.to_csv(featureName)
+    result.fillna(0, inplace = True)
+    return result
+    
+def extractX(X):
+    X.loc[ : , 'dayOfMonth'] = X['report_date'].map(lambda unit: unit.day)
+    X.loc[ : , 'dayOfWeek'] = X['report_date'].map(lambda unit: unit.weekday())
+    X.loc[ : , 'month'] = X['report_date'].map(lambda unit: unit.month)
+    
+    personDayGroup = X.groupby(['user_id', 'dayOfMonth'])
+    personDay = personDayGroup.mean()
+    personDay.drop(['dayOfWeek', 'month'], axis = 1, inplace = True)
+    
+    personGroup = X.groupby(['user_id'])
+    person = personGroup.mean()
+    person.drop(['dayOfMonth', 'dayOfWeek', 'month'], axis = 1, inplace = True)
+    
+    feature = pd.merge(personDay, person, how = 'left', left_index = True, right_index = True)
+    feature.fillna(0, inplace = True)
+    return feature
+    
 user_balance = pd.read_csv(user_balance_table, parse_dates = ['report_date'])
 user_balance.fillna(0, inplace = True)
 
@@ -37,42 +69,19 @@ train = extract(train_raw, train_target_raw)
 test = extract(test_raw, test_target_raw)
 
 X_train = train.ix[ : , : -2]
+X_train = MinMaxScaler().fit_transform(X_train)
 y_purchase = train.ix[ : , -2]
 y_redeem = train.ix[ : , -1]
 
-purchaseRegModel = LinearRegression(n_jobs = 2)
+X_train_test = test.ix[ : , : -2]
+X_train_test = MinMaxScaler().fit_transform(X_train_test)
+y_purchase_test = test.ix[ : , -2]
+y_redeem_test = test.ix[ : , -1]
+
+purchaseRegModel = Lasso()
 purchaseRegModel.fit(X_train, y_purchase)
-
-purchase
-
-def extract(X, y):
-    feature = extractX(X)
-    
-    y.loc[ : , 'dayOfMonth'] = y['report_date'].map(lambda unit: unit.day)
-    yPersonDayGroup = y.groupby(['user_id', 'dayOfMonth'])
-    target = yPersonDayGroup['total_purchase_amt', 'total_redeem_amt'].max()
-    
-    result = pd.merge(feature, target, how = 'right', left_index = True, right_index = True)
-    
-    #result.to_csv(featureName)
-    
-    return result
-    
-def extractX(X):
-    X.loc[ : , 'dayOfMonth'] = X['report_date'].map(lambda unit: unit.day)
-    X.loc[ : , 'dayOfWeek'] = X['report_date'].map(lambda unit: unit.weekday())
-    X.loc[ : , 'month'] = X['report_date'].map(lambda unit: unit.month)
-    
-    personDayGroup = X.groupby(['user_id', 'dayOfMonth'])
-    personDay = personDayGroup.mean()
-    personDay.drop(['dayOfWeek', 'month'], axis = 1, inplace = True)
-    
-    personGroup = X.groupby(['user_id'])
-    person = personGroup.mean()
-    person.drop(['dayOfMonth', 'dayOfWeek', 'month'], axis = 1, inplace = True)
-    
-    feature = pd.merge(personDay, person, how = 'left', left_index = True, right_index = True)
-    
-    return feature
+print (mean_squared_error(y_purchase, purchaseRegModel.predict(X_train)))
+print ()
+print (mean_squared_error(y_purchase_test, purchaseRegModel.predict(X_train_test)))
     
     
